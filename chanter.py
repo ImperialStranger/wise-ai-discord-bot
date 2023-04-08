e1a8934eb298a7b174265476df4f83975f14ca52
Nabil Sadik <34884277+ImperialStranger@users.noreply.github.com>
2023-04-08 11:36:45 +0600

    Update chanter.py

--- chanter.py
+++ chanter.py
@@ -10,6 +10,8 @@ intents.members = True
 
 client = discord.Client(intents=intents)
 
+previous_messages = {}
+
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
@@ -18,17 +20,27 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
+    
+    # Get the user's ID
+    user_id = message.author.id
 
     if isinstance(message.channel, discord.DMChannel):
-        response = generate_response(message.content)
+        response = generate_response(message.content, user_id)
         await message.channel.send(response)
         
     # Check if the bot is mentioned in the message
     elif client.user in message.mentions:
-        response = generate_response(message.content)
+        response = generate_response(message.content, user_id)
         await message.channel.send(response)
 
 def generate_response(prompt):
+    
+    # Get the user's previous message
+    previous_message = previous_messages.get(user_id, "")
+    
+    # Combine the previous message and the current prompt
+    prompt = f"{previous_message} {prompt}" if previous_message else prompt
+    
     # Call OpenAI's API to generate a response based on the prompt
     model_engine = "text-davinci-003"
     response = openai.Completion.create(
@@ -39,6 +51,9 @@ def generate_response(prompt):
         stop=None,
         temperature=0.9,
     )
+    
+    # Save the current message as the user's previous message
+    previous_messages[user_id] = prompt
 
     return response.choices[0].text.strip()
 
